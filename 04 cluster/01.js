91fd2447485745302ed68204becc48946765926d
trc-forever <921255465@qq.com>
2016-11-24 09:34:32 +0800

    16.11.24

--- 04 cluster/01.js
+++ 04 cluster/01.js
@@ -1,7 +1,7 @@
-使用所有cpu创建服务器监听9000端口
-var cluster = require('cluster');
-var http = require('http');
-var numCPUs = require('os').cpus().length;
+//使用所有cpu创建服务器监听9000端口
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
 //主进程cluster.isMaster返回true，cluster.isWorker返回false;子进程则相反
 if (cluster.isMaster) {
     console.log("master start...");
@@ -11,16 +11,17 @@ if (cluster.isMaster) {
         cluster.fork(); //创建与主进程一样的子进程
     }
 
-    cluster.on('listening', function(worker, address) { //当监听时的回调
+    cluster.on('listening', (worker, address) => { //当监听时的回调
         console.log('listening: worker ' + worker.process.pid + ', Address: ' + address.address + ":" + address.port);
     });
 
-    cluster.on('exit', function(worker, code, signal) { //子进程死亡时的回调
+    cluster.on('exit', (worker, code, signal) => { //子进程死亡时的回调
         console.log('worker ' + worker.process.pid + ' died');
     });
 } else {
-    http.createServer(function(req, res) {
-        res.writeHead(200);
-        res.end("hello world\n");
+    http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello World!');
     }).listen(9000);
 }
