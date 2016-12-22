f995df35203e5df7a786cb807edce17f398ad3ff
trc-forever <921255465@qq.com>
2016-12-22 09:10:13 +0800

    .

---
+++ 04 Koa/07/start.js
@@ -0,0 +1,7 @@
+require("babel-core/register")({
+    presets: ['stage-3', 'es2015']
+});
+
+require("babel-polyfill");
+
+require("./app.js");
--- 04 Koa/README.md
+++ 04 Koa/README.md
@@ -119,3 +119,4 @@ $ npm install
 $ npm start
 ```
 此时访问3000端口就可以了。
+
