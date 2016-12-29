aa53e6c871577baac9203b6b5aff1b143540020b
trc-forever <921255465@qq.com>
2016-12-29 10:08:35 +0800

    https

--- 01 http(s)/https/app.js
+++ 01 http(s)/https/app.js
@@ -2,11 +2,13 @@ const https = require('https');
 const fs = require('fs');
 
 const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('key-cert.pem')
+  key: fs.readFileSync('./key.pem'),
+  cert: fs.readFileSync('./key-cert.pem')
 };
 
 https.createServer(options, (req, res) => {
   res.writeHead(200);
   res.end('hello world\n');
-}).listen(9000);
+}).listen(9000, () => {
+  console.log('Server running ar port 9000.')
+});
